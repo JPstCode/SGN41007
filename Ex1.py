ecb4c44a72fc2c0994cda52e4cc8950b6b8a3b2b
Juho Pastell <juho.pastell@gmail.com>
2019-01-10 19:59:55 +0200

    part 4 + 5 done

--- Ex1.py
+++ Ex1.py
@@ -2,13 +2,14 @@
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from scipy.io import loadmat
 
 #Part 1
 fig = plt.figure()
 X = []
 
 csv = np.loadtxt("EX1/locationData.csv")
-print(csv.shape)
+#print(csv.shape)
 
 
 #Part 2
@@ -31,3 +32,42 @@ with open('EX1/locationData.csv') as fp:
 
 X = np.asarray(X)
 
+
+#Part 4
+
+mat = loadmat('EX1/twoClassData.mat')
+#print(mat.keys())
+
+xmat = mat['X']
+y = mat['y'].ravel()
+
+zeros = xmat[y == 0, :]
+ones = xmat[y == 1, :]
+
+#print(xmat[0:4])
+#print((xmat[:,1][0:4]))
+#print(xmat[:,0][0:4])
+#print(zeros[0:4])
+#print(ones[0:4])
+
+plt.figure(2)
+#plt.plot(xmat[:,0],xmat[:,1], 'ro' )
+plt.plot(zeros[:,0], zeros[:,1], 'ro')
+plt.plot(ones[:,0], ones[:,1], 'bo')
+#plt.show()
+
+#prt 5
+
+xs = np.load('EX1/x.npy')
+ys = np.load('EX1/y.npy')
+
+#line => y = mx + c ==>> y = Ap (A = [[x 1]], p = [[m],[c]]
+
+A = np.vstack([xs, np.ones(len(xs))]).T
+
+m, c = np.linalg.lstsq(A, ys)[0]
+
+plt.figure(3)
+plt.plot(xs,ys, 'o', label='original data', markersize=5)
+plt.plot(xs, m*xs + c, 'r', label='fittedline')
+plt.show()
