cab735046d7a3e9b98e5c3ae71c66116d0f3e803
Juho Pastell <juho.pastell@gmail.com>
2019-01-10 11:07:20 +0200

    parts 1-3 done

--- Ex1.py
+++ Ex1.py
@@ -1,4 +1,33 @@
 #First assignment
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
-print("hello world")
+#Part 1
+fig = plt.figure()
+X = []
+
+csv = np.loadtxt("EX1/locationData.csv")
+print(csv.shape)
+
+
+#Part 2
+
+#plt.plot(csv[0],csv[1])
+ax = fig.add_subplot(111, projection="3d")
+plt.plot(csv[0],csv[1],csv[2])
+
+#plt.show()
+
+
+#Part 3
+
+with open('EX1/locationData.csv') as fp:
+    for line in fp:
+        values = line.split(" ")
+        values = [float(v) for v in values]
+        X.append(values)
+
+
+X = np.asarray(X)
+
